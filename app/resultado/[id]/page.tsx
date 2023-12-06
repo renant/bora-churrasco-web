b22f9d35cdd3abdd73b32b01146091aafcb1f7df
Renan Teixeira <renan_replay@live.com>
2023-12-05 23:12:39 -0300

    add default pages results

---
+++ app/resultado/[id]/page.tsx
@@ -0,0 +1,87 @@
+import Result from '@/components/ui/result'
+import { getRandomAdsContent } from '@/services/ad-service'
+import Image from 'next/image'
+
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const participante = parseInt(params.id)
+
+  return {
+    title: `Cálculo De Churrasco Para ${participante} Pessoas`,
+    description: `Lista de compras e calculo estimado para um churrasco de ${participante} pessoas`,
+    keywords: ['Calculadora de Churraso', 'Churrasco', 'Calculadora'],
+    images: [
+      {
+        url: 'https://www.borachurrasco.app/images/ms-icon-310x310.png',
+      },
+    ],
+    openGraph: {
+      title: `Cálculo De Churrasco Para ${participante} Pessoas`,
+      description: `Lista de compras e calculo estimado para um churrasco de ${participante} pessoas`,
+      url: `https://www.borachurrasco.app/resultado/${participante}`,
+      images: [
+        {
+          url: 'https://www.borachurrasco.app/images/ms-icon-310x310.png',
+        },
+      ],
+      locale: 'pt_BR',
+      type: 'website',
+    },
+  }
+}
+
+export async function generateStaticParams() {
+  return [
+    { id: '5' },
+    { id: '10' },
+    { id: '15' },
+    { id: '20' },
+    { id: '25' },
+    { id: '30' },
+    { id: '35' },
+    { id: '40' },
+    { id: '50' },
+  ]
+}
+
+export default async function Resultado({
+  params,
+}: {
+  params: { id: string }
+}) {
+  const participante = parseInt(params.id)
+
+  const ads = await getRandomAdsContent()
+
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-16">
+        <h1 className="mb-2 text-center text-lg leading-relaxed text-orange-300 sm:text-4xl md:leading-snug">
+          Cálculo De Churrasco Para {participante} Pessoas
+        </h1>
+        <div className="flex flex-col  md:flex-row">
+          <div className="flex flex-col pr-8">
+            <Result participantes={participante} />
+          </div>
+          <div className="wrapper max-w-[300px] overflow-hidden rounded-b-md bg-gray-50  shadow-lg">
+            <div>
+              <Image src={ads.image} height={400} width={400} alt={ads.alt} />
+            </div>
+            <div className="p-3">
+              <h3 className="text-md m-0 font-semibold text-gray-700">
+                {ads.alt}
+              </h3>
+              <p className="leading-sm text-sm text-gray-900">
+                {ads.description}
+              </p>
+            </div>
+            <a href={ads.link} target="_blanck" className="no-underline">
+              <button className="flex w-full justify-center bg-red-600 py-2 font-semibold text-white transition duration-300 hover:bg-red-500">
+                Adquira já
+              </button>
+            </a>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
---
+++ components/ui/result.tsx
@@ -0,0 +1,217 @@
+'use client'
+import { Button, buttonVariants } from '@/components/ui/button'
+import churrascoStore from '@/lib/churrascoStore'
+import { useRouter } from 'next/navigation'
+import { useCallback, useEffect } from 'react'
+
+enum TipoMedida {
+  peso,
+  liquido,
+}
+
+function getMedida(value: number, tipo: TipoMedida) {
+  switch (tipo) {
+    case TipoMedida.peso: {
+      return value! >= 1000 ? `${value / 1000}kg` : `${value}g`
+    }
+    case TipoMedida.liquido: {
+      return value! >= 1000 ? `${value / 1000}L` : `${value}ml`
+    }
+  }
+}
+
+interface ResultProps {
+  participantes?: number | undefined
+}
+
+export default function Result({ participantes }: ResultProps) {
+  const router = useRouter()
+
+  const {
+    getTempo,
+    calcular,
+    bovina,
+    suina,
+    linguica,
+    frango,
+    queijo,
+    paoDeAlho,
+    temAssados,
+    temBebidas,
+    cerveja,
+    agua,
+    refrigerante,
+    suco,
+    temParticipantes,
+    assadosCalculados,
+    bebidasCalculadas,
+    essenciaisCalculados,
+    resetState,
+    createDefaultResult,
+  } = churrascoStore()
+
+  useEffect(() => {
+    if (participantes && participantes > 0) {
+      createDefaultResult(participantes)
+    }
+
+    if (!temParticipantes()) {
+      router.push('/')
+    } else {
+      calcular()
+    }
+  }, [calcular, temParticipantes, router, createDefaultResult, participantes])
+
+  const handleCalcularNovamente = useCallback(() => {
+    resetState()
+    router.push('/')
+  }, [resetState, router])
+
+  if (!temParticipantes()) {
+    return <></>
+  }
+
+  return (
+    <>
+      <div className="flex flex-col">
+        <h2 className="mb-2 text-center text-lg leading-relaxed text-orange-300 sm:text-4xl md:leading-snug">
+          Lista de Compras!
+        </h2>
+        <h3 className="text-md mb-2 text-center leading-relaxed text-orange-300 md:text-lg md:leading-snug">
+          Lembrando que o resultado é estimado para até {getTempo()} de comes e
+          bebes
+        </h3>
+        <h4 className="md:text-md mb-2 text-center text-sm font-thin text-orange-300 md:leading-snug">
+          (Isso é uma estimativa a quantidade pode variar, conheça seus
+          convidados)
+        </h4>
+
+        <div className="mt-4 flex flex-col items-start">
+          {assadosCalculados && (
+            <div>
+              <h5 className="text-md my-2 leading-relaxed text-orange-300 md:text-lg md:leading-snug">
+                Assados
+              </h5>
+              <ul className="list-disc pl-8">
+                {bovina && (
+                  <li className="md:text-md text-sm leading-relaxed text-orange-300 md:leading-snug">
+                    Bovina:{' '}
+                    {getMedida(assadosCalculados!.bovina, TipoMedida.peso)}
+                  </li>
+                )}
+                {suina && (
+                  <li className="md:text-md text-sm leading-relaxed text-orange-300 md:leading-snug">
+                    Suina:{' '}
+                    {getMedida(assadosCalculados!.suina, TipoMedida.peso)}
+                  </li>
+                )}
+                {linguica && (
+                  <li className="md:text-md text-sm leading-relaxed text-orange-300 md:leading-snug">
+                    Linguiça:{' '}
+                    {getMedida(assadosCalculados!.linguica, TipoMedida.peso)}
+                  </li>
+                )}
+                {frango && (
+                  <li className="md:text-md text-sm leading-relaxed text-orange-300 md:leading-snug">
+                    Frango:{' '}
+                    {getMedida(assadosCalculados!.frango, TipoMedida.peso)}
+                  </li>
+                )}
+                {queijo && (
+                  <li className="md:text-md text-sm leading-relaxed text-orange-300 md:leading-snug">
+                    Queijo:{' '}
+                    {getMedida(assadosCalculados!.queijo, TipoMedida.peso)}
+                  </li>
+                )}
+                {paoDeAlho && (
+                  <li className="md:text-md text-sm leading-relaxed text-orange-300 md:leading-snug">
+                    Pão de Alho:{' '}
+                    {getMedida(assadosCalculados!.paoAlho, TipoMedida.peso)}
+                  </li>
+                )}
+              </ul>
+            </div>
+          )}
+
+          {temBebidas() && bebidasCalculadas && (
+            <div>
+              <h5 className="text-md my-2 leading-relaxed text-orange-300 md:text-lg md:leading-snug">
+                Bebidas
+              </h5>
+              <ul className="list-disc pl-8">
+                {cerveja && (
+                  <li className="md:text-md text-sm leading-relaxed text-orange-300 md:leading-snug">
+                    Cerveja:{' '}
+                    {getMedida(bebidasCalculadas!.cerveja, TipoMedida.liquido)}
+                  </li>
+                )}
+                {refrigerante && (
+                  <li className="md:text-md text-sm leading-relaxed text-orange-300 md:leading-snug">
+                    Refrigerante:{' '}
+                    {getMedida(
+                      bebidasCalculadas!.refrigerante,
+                      TipoMedida.liquido,
+                    )}
+                  </li>
+                )}
+                {agua && (
+                  <li className="md:text-md text-sm leading-relaxed text-orange-300 md:leading-snug">
+                    Água:{' '}
+                    {getMedida(bebidasCalculadas!.agua, TipoMedida.liquido)}
+                  </li>
+                )}
+                {suco && (
+                  <li className="md:text-md text-sm leading-relaxed text-orange-300 md:leading-snug">
+                    Suco:{' '}
+                    {getMedida(bebidasCalculadas!.suco, TipoMedida.liquido)}
+                  </li>
+                )}
+              </ul>
+            </div>
+          )}
+
+          {(temAssados() || temBebidas()) && essenciaisCalculados && (
+            <div>
+              <h5 className="text-md my-2 leading-relaxed text-orange-300 md:text-lg md:leading-snug">
+                Essenciais
+              </h5>
+              <ul className="list-disc pl-8">
+                {temAssados() && (
+                  <li className="md:text-md text-sm leading-relaxed text-orange-300 md:leading-snug">
+                    Sal Grosso:{' '}
+                    {getMedida(
+                      essenciaisCalculados!.salGrosso,
+                      TipoMedida.peso,
+                    )}
+                  </li>
+                )}
+                {temAssados() && (
+                  <li className="md:text-md text-sm leading-relaxed text-orange-300 md:leading-snug">
+                    Carvão:{' '}
+                    {getMedida(essenciaisCalculados!.carvao, TipoMedida.peso)}
+                  </li>
+                )}
+                {temBebidas() && (
+                  <li className="md:text-md text-sm leading-relaxed text-orange-300 md:leading-snug">
+                    Gelo:{' '}
+                    {getMedida(essenciaisCalculados!.gelo, TipoMedida.peso)}
+                  </li>
+                )}
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="mt-4 flex flex-col justify-center pb-2">
+        <Button
+          onClick={() => handleCalcularNovamente()}
+          className={buttonVariants({ variant: 'outline' })}
+        >
+          {participantes && participantes > 0
+            ? 'Calcule novamente com mais precisão'
+            : 'Calcular novamente'}
+        </Button>
+      </div>
+    </>
+  )
+}
