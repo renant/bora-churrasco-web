026724fd6b364c53ffca06133628e7efa2921d8e
Renan Teixeira <renan_replay@live.com>
2025-07-07 15:03:00 -0300

    adicionando sugestoes no resultado calculado

--- app/resultado/page.tsx
+++ app/resultado/page.tsx
@@ -1,10 +1,16 @@
-import Result from '@/components/ui/result';
+import ClientSuggestedPosts from "@/components/client-suggested-posts";
+import ComoCalcularComponent from "@/components/como-calcular-component";
+import Result from "@/components/ui/result";
 
 export default function Resultado() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-16">
+      <main className="flex min-h-screen flex-col items-center justify-between p-0 md:p-16">
         <Result />
+        <div className=" mb-4 md:mb-12">
+          <ClientSuggestedPosts count={3} />
+        </div>
+        <ComoCalcularComponent />
       </main>
     </>
   );
